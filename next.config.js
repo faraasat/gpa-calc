3cd04c064cf3aff6d0c178fcc36b74f440813cd4
Farasat Ali <farasataliazeemi1@gmail.com>
2024-09-23 19:10:00 +0500

    (upd) [web] {misc} - add rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const optimize = require("next-optimized-classnames");
 
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap",
+        destination: "/sitemap.xml",
+      },
+    ];
+  },
 };
 
 module.exports = optimize(nextConfig);
