22660b26da5cc78f210933e94a4d2580e55ad5b7
Farasat Ali <farasataliazeemi1@gmail.com>
2024-10-27 22:13:47 +0500

    (upd) {misc} - misc updates

---
+++ components/ui/supported-countries.tsx
@@ -0,0 +1,10 @@
+const SupportedCountries = () => {
+    return <div>
+        <h3>Supported Countries Include</h3>
+        {
+            
+        }
+    </div>
+}
+
+export default SupportedCountries;
