c285034f530f398ff38c53bbacaf04fa8d52dc71
Farasat Ali <63093876+faraasat@users.noreply.github.com>
2025-05-14 11:14:14 +0500

    upd: navbar

--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -1,8 +1,8 @@
 import { NextComponentType } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { Fragment } from "react";
-// import { Menu } from "react-feather";
+import { Fragment, useState } from "react";
+import { IoMenu } from "react-icons/io5";
 
 const linkData = [
   {
@@ -29,6 +29,7 @@ const linkData = [
 
 const NavbarComponent: NextComponentType = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <Fragment>
@@ -37,7 +38,7 @@ const NavbarComponent: NextComponentType = () => {
           <Link href="/" className="font-potta text-2xl text-shadow-md">
             GPA CALCULATOR
           </Link>
-          <nav className="flex items-center justify-center gap-5">
+          <nav className="flex items-center justify-center gap-5 max-xl:hidden">
             {linkData.map((link) => (
               <Link
                 href={link.href}
@@ -52,6 +53,34 @@ const NavbarComponent: NextComponentType = () => {
               </Link>
             ))}
           </nav>
+          <div className="hidden max-xl:block">
+            <IoMenu
+              className="text-4xl cursor-pointer text-shadow-md"
+              onClick={() => setIsOpen((prev) => !prev)}
+            />
+            {isOpen && (
+              <nav className="fixed top-[70px] left-0 w-full bg-orange-c/50 flex items-center justify-center flex-col z-50 p-5 gap-3">
+                {linkData.map((link, i) => (
+                  <Fragment key={link.name}>
+                    <Link
+                      href={link.href}
+                      className={`font-potta text-2xl ${
+                        router && router?.pathname == link.href
+                          ? "text-shadow-lg"
+                          : ""
+                      }`}
+                      onClick={() => setIsOpen((prev) => !prev)}
+                    >
+                      {link.name}
+                    </Link>
+                    {i < linkData.length - 1 && (
+                      <hr className="w-full h-0.5 border-1 border-white" />
+                    )}
+                  </Fragment>
+                ))}
+              </nav>
+            )}
+          </div>
         </div>
       </header>
     </Fragment>
